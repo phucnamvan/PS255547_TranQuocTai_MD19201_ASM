ac1f22a23f5c367ecaa6ad87c7b1f76f5fa221f5
phucnamvan <tomheo11233@gmail.com>
2024-11-15 11:00:44 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { LogBox } from 'react-native'
 import Container from './src/navigations/Container'
 
 const App = () => {
+  console.log('tomheo');
   LogBox.ignoreAllLogs()
   return (
     <View style={{ flex: 1 }}>
